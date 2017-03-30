5b39416a65b9d6fda7123a92c113257e048ed9db
Ukidabek <ukidabek@gmail.com>
2017-03-30 20:22:03 +0200

    - GamePadInputSource added. * Base GamePad analog handling added.

---
+++ Assets/Logic/CrazyTankGameLogic/ImputSource/GamePadInputSource.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using BaseGameLogic.Inputs;
+
+namespace CrazyTank.Inputs
+{
+	public class GamePadInputSource : BaseInputSource 
+	{
+		public override InputSourceEnum InputSourceType 
+		{
+			get { return InputSourceEnum.GamePadInput; }
+		}
+
+		[SerializeField]
+		private AnalogInput _leftAnalog = new AnalogInput();
+
+		[SerializeField]
+		private AnalogInput _rightAnalog = new AnalogInput();
+
+		public override Vector3 MovementVector 
+		{
+			get { return base.MovementVector; }
+		}
+
+		public override Vector3 LookVector 
+		{
+			get { return base.LookVector; }
+		}
+
+		protected override void Awake ()
+		{
+			physicalInputs.Add (_leftAnalog);
+			physicalInputs.Add (_rightAnalog);
+
+			base.Awake ();
+		}
+	}
+}
