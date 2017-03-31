5f14460a9ff33c7d05852767b0ed92a353638116
Ukidabek <ukidabek@gmail.com>
2017-03-31 13:03:04 +0200

    - Driver logic implementation start.

---
+++ Assets/Logic/CrazyTankGameLogic/Controllers/CharacterControllers/DriverController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using BaseGameLogic.Character;
+
+namespace CrazyTank.Character
+{
+	public class DriverController : BasePlayerCharacterController
+	{
+	}
+}
---
+++ Assets/Logic/CrazyTankGameLogic/States/DefaultLocomotionState.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using BaseGameLogic.States;
+
+namespace CrazyTank.Character
+{
+	public class DefaultLocomotionState : BaseState 
+	{
+		protected DriverController driver = null;
+
+		public DefaultLocomotionState (
+			BaseStateObject controlledObject, 
+			BaseInputCache inputCache, 
+			BaseAnimationHandlingCache animationHandlingCache) 
+			: base (controlledObject, inputCache, animationHandlingCache)
+		{
+			if (controlledObject is DriverController) 
+			{
+				driver = ControlledObject as DriverController;
+			}
+		}
+		
+		public override void Enter ()
+		{
+		}
+
+		public override void Exit ()
+		{
+		}
+
+		public override void Sleep ()
+		{
+		}
+
+		public override void Awake ()
+		{
+		}
+
+		public override void OnUpdate ()
+		{
+		}
+
+		public override void OnLateUpdate ()
+		{
+		}
+
+		public override void OnFixedUpdate ()
+		{
+		}
+	}
+}
